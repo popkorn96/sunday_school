229f8589cf87e8d7f2c4f65f7a4b19e79c935689
popkorn96 <mpaloma2014@gmail.com>
2020-10-14 18:23:23 -0500

    added assignment controller actions

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -13,6 +13,31 @@ class AssignmentsController < ApplicationController
   end
 
   def edit
+    @assignment = Assignment.find(params[:id])
+  end
+
+  def create
+    @assignment = Assignment.new
+    if @assignment.save
+      redirect_to @assignment
+    else 
+      render:new
+    end
+  end
+
+  def update
+    @assignment = Assignment.find(params[:id])
+    if @assignment.update
+      redirect_to @assignment
+    else 
+      render:edit
+    end
+  end
+
+  private
+
+  def assign_params
+    params.require(:assignment).permit(:title, :description, :due_date, :teacher_id, :child_id)
   end
 
 end
